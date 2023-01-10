30592cf161cc94bcb8191a0a9e21b95f93b0afea
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2023-01-10 12:02:36 -0600

    add firebase hosting

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyC43kJtLnFG3FqPg4HQ0Ltcdh961M-j-Z0',
+  authDomain: 'front-challenge-561dd.firebaseapp.com',
+  projectId: 'front-challenge-561dd',
+  storageBucket: 'front-challenge-561dd.appspot.com',
+  messagingSenderId: '881077083625',
+  appId: '1:881077083625:web:75b47e7ab242d2fe5f3f78',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
